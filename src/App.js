de3c99c9b94c66bc475597e389b98ca8ffde1692
Daniel Perz <dpere316@fiu.edu>
2020-07-07 17:42:29 -0400

    Daniel Branch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/navbar'
-
+import React from "react";
+import "./App.css";
+import Navbar from "./components/navbar";
+import Header from "./components/header";
 
 function App() {
-
   return (
     <div className="App">
-    <Navbar/>
+      <Header />
     </div>
   );
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,42 +1,36 @@
-import React, {
-    Component
-} from "react";
+import React, { Component } from "react";
 import axios from "axios";
 const APP_ID = "1eddd451";
 const APP_KEY = "9acb3aa12defaeed4a85d8e39cddd73d";
 
 class navbar extends Component {
-    state = {
-        foods: [],
-    };
+  state = {
+    foods: {},
+  };
 
-    async componentDidMount() {
-        let res = await axios.get(
-            `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`
-        );
-        console.log(res);
-        this.setState({
-            foods: res.data,
-        });
-        // localStorage.setItem("res", JSON.stringify(res.data));
-    }
+  async componentDidMount() {
+    let res = await axios.get(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`
+    );
+    console.log(res);
+    this.setState({
+      foods: res.data,
+    });
+    // localStorage.setItem("res", JSON.stringify(res.data));
+  }
 
-    displayFoods = () => {
-        return this.state.foods.map((eachfood) => {});
-    };
+  displayFoods = () => {
+    return this.state.foods.map((eachfood) => {});
+  };
 
-    render() {
-        // console.log(JSON.parse(localStorage.getItem("res")));
-        // let obj = JSON.parse(localStorage.getItem("res"));
-        // This is Daniel Branch
-        return ( <
-            div >
-
-
-            <
-            /div>
-        );
-    }
+  render() {
+    // console.log(JSON.parse(localStorage.getItem("res")));
+    // let obj = JSON.parse(localStorage.getItem("res"));
+    // This is Daniel Branch
+    return <div>
+        
+    </div>;
+  }
 }
 
 export default navbar;
