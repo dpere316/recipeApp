a32191fe4c538578a019e76502df24027b062d1e
Daniel Perz <dpere316@fiu.edu>
2020-07-08 17:54:29 -0400

    imports fixed

--- src/components/Dinner.js
+++ src/components/Dinner.js
@@ -1,11 +1,5 @@
 import React, { Component } from "react";
-<<<<<<< HEAD
-import axios from "axios";
-const APP_ID = "1eddd451";
-const APP_KEY = "9acb3aa12defaeed4a85d8e39cddd73d";
-=======
 import axios from "axios"
->>>>>>> 02d47ec7b982026b6b93d3c054298dcef5c1becd
 
 
 class Dinner extends Component {
