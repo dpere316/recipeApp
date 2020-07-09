b293974d961fc33fedea489aed90f231d568e0e0
vlee13 <vlee13@gmail.com>
2020-07-09 15:18:26 -0700

    ethic foods update/not working

--- src/components/EthnicFood.js
+++ src/components/EthnicFood.js
@@ -1,9 +1,54 @@
 import React, { Component } from "react";
+import axios from "axios";
 
-class Personalize extends Component {
+const APP_ID2 = "ffd7e1b9";
+const APP_KEY2 = "e439b5df8590bafcf11efad43ca3a69b";
+
+class EthnicFood extends Component {
+  state = {
+    ethnicFoods: [
+      "Mexican",
+      "Japanese",
+      "Italian",
+      "American",
+      "Indian",
+      "Chinese",
+      "French",
+      "South American",
+      "Caribbean",
+      "Mediterranean",
+    ];
+  };
+
+  async componentDidMount() {
+    let res = await axios.get(
+      `https://api.edamam.com/search?q=${this.state.ethnicFoods}&to=100&app_id=${APP_ID2}&app_key=${APP_KEY2}`
+    );
+    // console.log("random food type category", res);
+
+    const food = this.randomizedFood(res.data.hits);
+    // console.log("random food within type category", food);
+
+    this.setState({
+      foods: [...this.state.foods, food.recipe],
+    });
+  }
+
   render() {
-    return <div></div>;
+    return <div>
+      <li>"Mexican"</li>
+      <li>"Japanese"</li>
+      <li>"Italian"</li>
+      <li>"American"</li>
+      <li>"Indian"</li>
+      <li>"Chinese"</li>
+      <li>"French"</li>
+      <li>"South American"</li>
+      <li>"Caribbean"</li>
+      <li>"Mediterranean"</li>
+    </div>;
   }
 }
 
-export default Personalize;
+
+export default EthnicFood;
