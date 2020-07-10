cd9a458f765e603d9eccd522f8c109f6466c309b
vlee13 <vlee13@gmail.com>
2020-07-10 10:00:18 -0700

    need newer version from master

--- src/components/Lunch.js
+++ src/components/Lunch.js
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.css";
-const APP_ID = "1eddd451";
-const APP_KEY = "9acb3aa12defaeed4a85d8e39cddd73d";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "../App.css";
+const APP_ID = "b479ca7f";
+const APP_KEY = "1091d11a059bf224db39af98bca9540f	";
+
+const settings = {
+  dots: false,
+  infinte: true,
+  speed: 10,
+  arrows: true,
+  slidesToScroll: 1,
+  slidesToShow: 5,
+  className: "slides",
+};
 
 class Lunch extends Component {
   state = {
@@ -28,7 +40,7 @@ class Lunch extends Component {
     let res = await axios.get(
       `https://api.edamam.com/search?q=${
         this.state.foodType[this.randomIndex()]
-      }+lunch&to=100&app_id=${APP_ID}&app_key=${APP_KEY}`
+      }+lunch&to=10&app_id=${APP_ID}&app_key=${APP_KEY}`
     );
     this.setState({
       foods: res.data.hits,
@@ -38,7 +50,16 @@ class Lunch extends Component {
 
   displayAllFoods = () => {
     return this.state.foods.map((eachFood) => {
-      return <div></div>;
+      return (
+        <div className="container">
+          <img
+            style={{ width: "100px" }}
+            src={eachFood.recipe.image}
+            alt={eachFood.recipe.label}
+          />
+          <p>{eachFood.recipe.label}</p>
+        </div>
+      );
     });
   };
 
@@ -50,7 +71,8 @@ class Lunch extends Component {
   render() {
     return (
       <div>
-        <div className="columns"> {this.displayAllFoods()}</div>
+        <h1>Lunch</h1>
+        <Slider {...settings}> {this.displayAllFoods()} </Slider>
       </div>
     );
   }
