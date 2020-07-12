cb5dbfd8c2b7d58d728578e783d310648ccb1b2e
Daniel Perz <dpere316@fiu.edu>
2020-07-12 12:22:12 -0400

    removing

--- src/components/RandomFood.js
+++ src/components/RandomFood.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-// const APP_ID = "b479ca7f";
-// const APP_KEY = "1091d11a059bf224db39af98bca9540f	";
+const APP_ID = "b479ca7f";
+const APP_KEY = "1091d11a059bf224db39af98bca9540f	";
 
 const APP_ID2 = "ffd7e1b9";
 const APP_KEY2 = "e439b5df8590bafcf11efad43ca3a69b";
@@ -43,7 +43,7 @@ class Home extends Component {
     let res = await axios.get(
       `https://api.edamam.com/search?q=${
         this.state.randomTypeFood[this.randomIndex()]
-      }&to=100&app_id=${APP_ID2}&app_key=${APP_KEY2}`
+      }&to=100&app_id=${APP_ID}&app_key=${APP_KEY}`
     );
     // console.log("random food type category", res);
 
