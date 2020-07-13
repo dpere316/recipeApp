9252714b86ddc38da005690a9fe596318a7022b8
vlee13 <vlee13@gmail.com>
2020-07-13 06:21:07 -0700

    nothing much

--- src/components/AllFoods.jsx
+++ src/components/AllFoods.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
 const AllFoods = (props) => {
   console.log(props);
   if (!props.foods.length) {
     props.history.push("/");
   }
+
   return (
     <div>
       {props.foods.map((item) => (
