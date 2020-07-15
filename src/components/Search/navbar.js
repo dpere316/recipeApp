18c1fe79355d4e2d0c1f432369360ecf293b4fcd
Daniel Perz <dpere316@fiu.edu>
2020-07-15 09:50:03 -0400

    merging from master after roy added navbar

--- src/components/Search/navbar.js
+++ src/components/Search/navbar.js
@@ -8,10 +8,10 @@ import Search from "../search";
 const APP_ID2 = "027c69d3";
 const APP_KEY2 = "06c10b8536afafcb35d471d1e71b262e";
 class navbar extends Component {
-  // state = {
-  //   foods: [],
-  //   search: "",
-  // };
+  state = {
+    foods: [],
+    search: "",
+  };
   // async componentDidMount() {
   //   let res = await axios.get(
   //     `https://cors-anywhere.herokuapp.com/https://api.edamam.com/search?q=${this.state.search}&to=100&app_id=${APP_ID2}&app_key=${APP_KEY2}`
